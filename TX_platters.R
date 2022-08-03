40c6dc5fb4fb1c892a9c7930659465e4998e91a3
dankatz <dankatz512@gmail.com>
2022-08-03 16:33:20 -0400

    starting to customize for TX platters

--- TX_platters.R
+++ TX_platters.R
@@ -28,27 +28,26 @@ here::i_am("katz_photo.jpg")
 # write.table(test4, "clipboard", sep="\t", row.names=FALSE, col.names=FALSE)
 
 
-### connect to google drive spreadsheet #######################################################################################
-deployment_sheet <- read_csv(here("Cornell", "Local projects", "twig pheno 2022", "deployment_sheet_220727b.csv"))%>% 
-  filter(!is.na(processed_file))
+### connect to deployment spreadsheet #######################################################################################
+deployment_sheet <- read_csv(here("texas", "pheno", "pollen_platter_analysis", "pollen_platters_fs20_21_220803.csv"))
 
-platters_to_process_list_rows <- c(25:48)
+platters_to_process_list_rows <- c(1:110)
 for(j in 1:length(platters_to_process_list_rows)){
-  
+  print(j)
   platter_row <- platters_to_process_list_rows[j] #platter_row <- platters_to_process_list_rows[1]
-  scanned_file_name_focal <- deployment_sheet$processed_file[platter_row]
+  scanned_file_name_focal <- deployment_sheet$scanned_file[platter_row]
   
   #sampler number
   sampler_n <- deployment_sheet$sampler[platter_row]  #"9"
   
-  #time start
-  time_start <- mdy_hm(deployment_sheet$sampler_start_date_time[platter_row])
+  # #time start
+  # time_start <- mdy_hm(deployment_sheet$sampler_start_date_time[platter_row])
   
   #time deployed
-  time_deploy <- mdy_hm(deployment_sheet$deployment_time[platter_row]) #mdy_hms("6/24/2022 14:59:00")
+  time_deploy <- mdy_hm(deployment_sheet$date_deploy_auto[platter_row]) #mdy_hms("6/24/2022 14:59:00")
   
   #time retreived
-  time_retreived <- mdy_hm(deployment_sheet$retrieval_time[platter_row]) #mdy_hms("6/26/2022 13:57:00")
+  time_retreived <- mdy_hm(deployment_sheet$date_retreive_auto[platter_row]) #mdy_hms("6/26/2022 13:57:00")
   
   #angle at retreival 
   #angle_retreival <- deployment_sheet$retreival_angle[platter_row] + 90 #the measured angle is from the blue line to the left side of the opening slit. The angles used in this script start at 90 #8 + 360
@@ -58,19 +57,19 @@ for(j in 1:length(platters_to_process_list_rows)){
   platter_centroid_y <- deployment_sheet$image_centroid_y[platter_row] # 4044
   
   #programmed step time
-  step_time_min <- 65.717 
+  step_time_min <- 112.4394 
   step_time_sec = step_time_min * 60
-  step_angle <- (12/516) * 365
+  step_angle <- (4/516) * 365
+  n_slots <- (360/step_angle) #* step_time_min)/60)/24
   
-  platter_df <- data.frame(time_period = 1:44, timestep_start = rep(NA, 44), timestep_end = rep(NA,44),
-                           angle_start = rep(NA, 44), angle_end = rep(NA, 44))
+  platter_df <- data.frame(time_period = 1:n_slots, timestep_start = rep(NA, n_slots), timestep_end = rep(NA,n_slots),
+                           angle_start = rep(NA, n_slots), angle_end = rep(NA, n_slots))
   
   
   platter_df <- platter_df %>% 
     mutate(timestep_start = time_deploy + step_time_sec * time_period - step_time_sec,
-           timestep_end = step_time_min - (as.numeric(difftime(time_deploy, time_start, units = "mins")) %% step_time_min) + #time left before turn on deploy
-             time_deploy + 
-             step_time_sec * time_period - step_time_min,
+           timestep_end = #step_time_min - (as.numeric(difftime(time_deploy, time_start, units = "mins")) %% step_time_min) + #time left before turn on deploy
+                          time_deploy + step_time_sec * time_period - step_time_min,
            angle_start = -step_angle * time_period - step_angle + (90 + step_angle*2), #the blue line starts on the left hand side of the open slit, so the first slot is just over 90 degrees
            angle_end = -step_angle * time_period + (90 + step_angle *2))
   # seg_obj_n = NA,
@@ -82,8 +81,8 @@ for(j in 1:length(platters_to_process_list_rows)){
   # file_path <- "C:/Users/dsk273/Box/Cornell/mentoring/student projects/summer 2022/Kent pollen catcher/platter_scans/processed_scan/"
   #here("Cornell", "mentoring", "student projects", "summer 2022", "Kent pollen catcher", "platter_scans", "processed_scan", "pp_scan_sampler9_d220624_r.tif")
   #rotated_image_r <- raster::stack("C:/Users/dsk273/Desktop/pp_scan_sampler_14_d220510_c.tif")
-  rotated_image <- terra::rast(here("Cornell", "Local projects", "twig pheno 2022", "scanned files", "processed_scan", 
-                                    paste0(scanned_file_name_focal, ".tif")
+  rotated_image <- terra::rast(here("texas", "pollen platter", "TX_platter_scans", "processed_scans", 
+                                    paste0(scanned_file_name_focal, "_c.tif")
                                     #"pp_scan_sampler9_d220624_r.tif"
   )
   )
@@ -111,15 +110,16 @@ for(j in 1:length(platters_to_process_list_rows)){
       inner_points_x_end   = platter_centroid_x + platter_dist_inner * cos(NISTdegTOradian(platter_df$angle_end)),#inner points of the focal slot
       inner_points_y_end  = platter_centroid_y + platter_dist_inner * sin(NISTdegTOradian(platter_df$angle_end)))
   
+  plot(rotated_image) #plot(1:1)
   #loop through each individual time chunk and save a file for it
-  for(i in 1:42){ #max 42 non-overlapping strips
+  for(i in 1:n_slots){ #max 42 non-overlapping strips
     #create a spatial vector out of the points and visual check
     pp_aoi <- rbind(c(platter_df$outer_points_x_start[i], platter_df$outer_points_y_start[i]), 
                     c(platter_df$inner_points_x_start[i], platter_df$inner_points_y_start[i]), 
                     c(platter_df$inner_points_x_end[i], platter_df$inner_points_y_end[i]), 
                     c(platter_df$outer_points_x_end[i], platter_df$outer_points_y_end[i]))
     pp_aoi_v <- vect(pp_aoi, type = "polygons")
-    plot(rotated_image) #plot(1:1)
+    # plot(rotated_image) #plot(1:1)
     plot(pp_aoi_v, col='yellow', add = TRUE)
     
     #extract image
@@ -132,7 +132,7 @@ for(j in 1:length(platters_to_process_list_rows)){
     file_save_name <- paste0(gsub(x = scanned_file_name_focal, pattern = ".tif", replacement = ""), "_chunk_", i, ".tif")
     terra::writeRaster(chunk_x, 
                        
-                       here("Cornell",  "Local projects", "twig pheno 2022", "scanned files", "platter_chunks", 
+                       here("texas", "pollen platter", "TX_platter_scans", "processed_scans", "platter_chunks", 
                             file_save_name),
                        overwrite = TRUE)
   } #end loop for 
